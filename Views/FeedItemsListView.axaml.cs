0434777cb57d79555fbca10e5af4d411a6752c7d
Nisar Ahmad <nisarahmad1324@gmail.com>
2023-11-20 16:04:31 +0530

    Refactor the listview into separate user control

---
+++ Views/FeedItemsListView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Skimmer.Avalonia.Views;
+
+public partial class FeedItemsListView : UserControl
+{
+    public FeedItemsListView()
+    {
+        InitializeComponent();
+    }
+}
